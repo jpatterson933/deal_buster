20fa0575535328977b55ee1c5cb796f8f04893dc
Jeff <jpatterson933@ucla.edu>
2022-04-08 19:18:14 -0700

    added another faq question

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -8,6 +8,8 @@ export const Faq = () => {
     return (
         <div id="faq">
             <h1>Frequently Asked Questions</h1>
+            <h3>What do you sell?</h3>
+            <p>We sell a variety of overstock and returns from major retailers.</p>
             <h3>Can I return or exchange stuff I bought?</h3>
             <p>No returns or exchanges</p>
             <h3>What forms of Payment do you accept?</h3>
